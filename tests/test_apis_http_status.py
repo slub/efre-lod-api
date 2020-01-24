47aa6a8678117cc557dca2ecddab78f34641600f
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-01-24 09:47:38 +0100

    fixed test:
     - use config file for obtaining ID of dataset
     - Raise Exception upon failed API request

--- tests/test_apis_http_status.py
+++ tests/test_apis_http_status.py
@@ -1,6 +1,5 @@
 import pytest
 import requests
-import re
 
 import lod_api
 from .http_status import HttpStatusBase
@@ -26,6 +25,9 @@ class TestHttpStatusEndpoints(HttpStatusBase):
             search_url = self.host + "/{entity}/search".format(entity=index)
             print(search_url)
             search_res = requests.get(search_url)
+            if not search_res.ok:
+                raise Exception("Could not retrieve result for URL=\'{}\'"
+                                .format(search_url))
 
             # get first dataset
             for res_json in search_res.json()[0:test_count]:
@@ -49,6 +51,9 @@ class TestHttpStatusEndpoints(HttpStatusBase):
 
             print(search_url)
             search_res = requests.get(search_url)
+            if not search_res.ok:
+                raise Exception("Could not retrieve result for URL=\'{}\'"
+                                .format(search_url))
 
             # get first dataset
             for res_json in search_res.json()[0:test_count]:
@@ -64,37 +69,32 @@ class TestHttpStatusEndpoints(HttpStatusBase):
 
         test_count = 1
 
-        url_schema = {"gnd": "d-nb.info/gnd",
-                      "swb": "swb.bsz-bw.de",
-                      "viaf": "viaf.org",
-                      "wd": "wikidata"
-                      }
+        url_schema = lod_api.CONFIG.get("authorities")
 
-        for authority in lod_api.CONFIG.get("authorities"):
+        for authority in url_schema:
             for entity in lod_api.CONFIG.get("indices_list"):
                 # request to get id from one dataset
                 search = url_schema[authority]
                 search_url = (self.host
-                              + "/{entity}/search?q=sameAs:*\"{search}\""
+                              + "/{entity}/search?q=sameAs.@id:\"{search}\""
                               .format(entity=entity, search=search))
 
                 print(search_url)
                 search_res = requests.get(search_url)
+                if not search_res.ok:
+                    raise Exception("Could not retrieve result for URL=\'{}\'"
+                                    .format(search_url))
 
-                # get first dataset, or all
                 for res_json in search_res.json()[0:test_count]:
                     # get ID of first dataset (without rest of URI)
-                    # Problem: for each authority provider the URI looks different
-                    #          as well as the form of the ID
 
-                    # there is a wikidata id: http://www.wikidata.org/entity/Q54828
-                    # which has five digits which seems to be the lower bound
-                    pattern = re.compile("(Q[0-9]{1,}|[0-9-]{5,}X?)")
                     auth_id = None
                     for item in res_json["sameAs"]:
-                        if url_schema[authority] in item:
-                            print("authority-item: ", item)
-                            auth_id = pattern.search(item).group()
+                        if url_schema[authority] in item["@id"]:
+                            print("authority-item: ", item["@id"])
+                            # hence we've configured the IDs in the config, we split
+                            # the item with the base_uri and get the ID with the 2nd slice
+                            auth_id = item["@id"].split(url_schema[authority])[1]
                     if not auth_id:
                         continue
 
