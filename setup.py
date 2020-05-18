5f87f363a47ac7b1b318e7728ad2955fe069d949
Tom Schilling <Tom.Schilling@slub-dresden.de>
2020-05-18 16:17:49 +0200

    v0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='lod-api',
-    version='0.4.0',
+    version='0.5.0',
     description='API to provide LOD from Elasticsearch via REST interface',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
