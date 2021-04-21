0defd8325a4cf41efb6cf780102bc8dcb1a026e2
Tom Schilling <Tom.Schilling@slub-dresden.de>
2021-04-22 00:16:49 +0200

    extended schema to use with aggregation

--- src/lod_api/apis/explore_schema.py
+++ src/lod_api/apis/explore_schema.py
@@ -9,12 +9,35 @@ _additionalType = Schema({
     Optional('id'): str
     })
 
+_topAuthor = Schema({
+    'key': str,
+    'doc_count': int
+    })
+
+_datePublished = Schema({
+    'year': int,
+    'count': int
+    })
+
+_mention = Schema ({
+    'name': str,
+    'docCount': int
+    })
+
+_resourceAggregation = Schema({
+    'docCount': int,
+    'topAuthors': [_topAuthor],
+    'datePublished': [_datePublished],
+    'mentions': [_mention]
+    })
+
 topicsearch = Schema({
     'id': str,
     'name': str,
     Optional('alternateName',   default=[]): [str],
     Optional('description',     default=""): str,
     Optional('score',           default=0):  float,
-    Optional('additionalTypes', default=[]): [_additionalType]
+    Optional('additionalTypes', default=[]): [_additionalType],
+    Optional('aggregations'): _resourceAggregation,
     })
 
