79f9729d368bd61128197643a3122aa936dca5e3
Tom Schilling <Tom.Schilling@slub-dresden.de>
2021-07-19 16:25:18 +0200

    reordered elasticsearch queries

--- src/lod_api/apis/explore_queries.py
+++ src/lod_api/apis/explore_queries.py
@@ -59,34 +59,82 @@ _fields_aggs = ['preferredName^2',
                 'about.keywords'
                ]
 
-def topic_maggs_query_topicMatch(subjects):
-    """
-    Creates a aggregation query "topicAM" which produces a
-    adjacency matrix with different subject correlations in its
-    fields.
-    :param list subjects - list of subjects
-
-    :returns: tuple of aggregation name together with aggregation query
-    to use with elasticsearch
-    """
-    query = {
-            "size": 0,
-            "aggs": {
-                "topicAM": {
-                    "adjacency_matrix": {
-                        "filters": {
-                            subj: {
-                                "terms": {
-                                    "mentions.name.keyword": [subj]
-                                }
-                            }
-                            for subj in subjects
-                        }
-                    }
+### generic topicsearch query ###
+#
+#
+def topic_query(query, q_size, q_fields, q_excludes, q_from=0):
+    # TOOD: multi_match types best_fields, most_fields, cross_fields, phrase, phrase_prefix, bool_prefix
+    # https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html
+    # multi_match:
+    #   query,
+    #   fields: ['*'],
+    #
+    # type: 'most_fields'
+    # }
+    return {
+        'size': q_size,
+        'from': q_from,
+        '_source': q_excludes,
+        'query': {
+            "multi_match": {
+                'query':  query,
+                'fields': q_fields,
+                'type': 'phrase'
                 }
             }
         }
-    return query
+
+### phraseMatch queries ###
+#
+#
+def topic_aggs_query_phraseMatch(query, filter1=None):
+    """
+    Creates elasticsearch query for aggregations based on a simple
+    phrase search for different subjects contained in `query`.
+    :param list query
+    :param str filter1 - filter query that is used for author names
+
+    :returns: dict elasticsearch query body
+    """
+    if isinstance(query, str):
+        subjects = [query]
+    elif isinstance(query, list):
+        subjects = query
+    else:
+        raise Exception("query should be of type str or list")
+
+    if filter1:
+        author_filter = [{
+                "multi_match": {
+                    "fields": [
+                        "author.name.keyword",
+                        "contributor.name.keyword"
+                        ],
+                    "query": filter1
+                    }
+                }]
+    else:
+        author_filter = []
+
+    return {
+        "size": 15,
+        "sort": _sort_aggs,
+        "query": {
+            "bool": {
+                "must" : [
+                    {
+                        "multi_match": {
+                            "query": subj,
+                            "fields": _fields_aggs,
+                            "type": "phrase"
+                            }
+                        }
+                    for subj in subjects],
+                "filter": author_filter
+                }
+            },
+        "aggs": _aggs
+        }
 
 def topic_maggs_query_phraseMatch(subjects):
     """
@@ -98,6 +146,7 @@ def topic_maggs_query_phraseMatch(subjects):
     :returns: tuple of aggregation name together with aggregation query
     to use with elasticsearch
     """
+
     query = {
             "size": 0,
             "aggs": {
@@ -128,7 +177,20 @@ def topic_maggs_query_phraseMatch(subjects):
         }
     return query
 
+
+### topicMatch queries ###
+#
+#
 def topic_aggs_query_topicMatch(query, filter1=None):
+    """
+    Creates elasticsearch query for aggregations based on filter
+    restricting the query to specific topic names. Query subjects are
+    given in `query` parameter.
+    :param list query
+    :param str filter1 - filter query that is used for author names
+
+    :returns: dict elasticsearch query body
+    """
     if isinstance(query, str):
         subjects = [query]
     elif isinstance(query, list):
@@ -178,68 +240,34 @@ def topic_aggs_query_topicMatch(query, filter1=None):
         "aggs": _aggs
         }
 
-def topic_aggs_query_phraseMatch(query, filter1=None):
-    if isinstance(query, str):
-        subjects = [query]
-    elif isinstance(query, list):
-        subjects = query
-    else:
-        raise Exception("query should be of type str or list")
+def topic_maggs_query_topicMatch(subjects):
+    """
+    Creates a aggregation query "topicAM" which produces a
+    adjacency matrix with different subject correlations in its
+    fields.
+    :param list subjects - list of subjects
 
-    if filter1:
-        author_filter = [{
-                "multi_match": {
-                    "fields": [
-                        "author.name.keyword",
-                        "contributor.name.keyword"
-                        ],
-                    "query": filter1
-                    }
-                }]
-    else:
-        author_filter = []
-
-    return {
-        "size": 15,
-        "sort": _sort_aggs,
-        "query": {
-            "bool": {
-                "must" : [
-                    {
-                        "multi_match": {
-                            "query": subj,
-                            "fields": _fields_aggs,
-                            "type": "phrase"
+    :returns: tuple of aggregation name together with aggregation query
+    to use with elasticsearch
+    """
+    query = {
+            "size": 0,
+            "aggs": {
+                "topicAM": {
+                    "adjacency_matrix": {
+                        "filters": {
+                            subj: {
+                                "terms": {
+                                    "mentions.name.keyword": [subj]
+                                }
                             }
+                            for subj in subjects
                         }
-                    for subj in subjects],
-                "filter": author_filter
-                }
-            },
-        "aggs": _aggs
-        }
-
-def topic_query(query, q_size, q_fields, q_excludes, q_from=0):
-    # TOOD: multi_match types best_fields, most_fields, cross_fields, phrase, phrase_prefix, bool_prefix
-    # https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html
-    # multi_match:
-    #   query,
-    #   fields: ['*'],
-    #
-    # type: 'most_fields'
-    # }
-    return {
-        'size': q_size,
-        'from': q_from,
-        '_source': q_excludes,
-        'query': {
-            "multi_match": {
-                'query':  query,
-                'fields': q_fields,
-                'type': 'phrase'
+                    }
                 }
             }
         }
+    return query
 
 def topic_resource_mentionCount(topic_id):
     return {
